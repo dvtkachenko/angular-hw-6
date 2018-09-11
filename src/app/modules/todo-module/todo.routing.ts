109b20f7e9972536b8ae0625447b325435ef6545
Dmytro Tkachenko <dt@pcsoft.de>
2018-09-11 14:10:29 +0200

    fixed some issue

--- src/app/modules/todo-module/todo.routing.ts
+++ src/app/modules/todo-module/todo.routing.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { TodoListComponent, TodoAddComponent, TodoEditComponent } from './components';
-import { AuthGuard } from '../../core';
 
 export const todoRoutes: Routes = [
     { path: '', component: TodoListComponent },
